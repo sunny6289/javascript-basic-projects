36ffd9766e4b29c58d8d57f79ba302078ba75d5f
Saikat Das <saikatdas40g@gmail.com>
2023-10-10 23:43:59 +0530

    fixed music bug

--- snake game/js/index.js
+++ snake game/js/index.js
@@ -1,9 +1,9 @@
 // Game constants and Variables
 let inputDir = {x:0,y:0};
-const foodSound = new Audio('../music/food.mp3');
-const gameOverSound = new Audio('../music/gameover.mp3');
-const moveSound = new Audio('../music/move.mp3');
-const musicSound = new Audio('../music/music.mp3');
+const foodSound = new Audio('../../snake game/music/food.mp3');
+const gameOverSound = new Audio('../../snake game/music/gameover.mp3');
+const moveSound = new Audio('../../snake game/music/move.mp3');
+const musicSound = new Audio('../../snake game/music/music.mp3');
 let lastPaintTime = 0;
 let speed = 2;
 let score = 0;
